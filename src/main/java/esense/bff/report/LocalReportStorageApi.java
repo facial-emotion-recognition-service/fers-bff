986b8e20fce8ae68f6638ce4b61d33e1b5c747e2
Or Bartal <orbartal@gmail.com>
2023-05-25 14:15:05 -0400

    Add logs to

--- src/main/java/esense/bff/report/LocalReportStorageApi.java
+++ src/main/java/esense/bff/report/LocalReportStorageApi.java
@@ -1,10 +1,13 @@
 package esense.bff.report;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,15 +36,21 @@ public class LocalReportStorageApi {
 	private void init() {
 		dirValidator.createIfNotExists(reportsDirPath);
 		dirValidator.validateDirPath(reportsDirPath);
-		logger.info("LocalReportStorageApi.init: reportsDirPath " + reportsDirPath);
+		File dir = new File(reportsDirPath);
+		logger.info("LocalReportStorageApi.init 1: reportsDirPath " + reportsDirPath);
+		logger.info("LocalReportStorageApi.init 2: dir exists = " + dir.exists());
+		logger.info("LocalReportStorageApi.init 3: dir isDirectory = " + dir.isDirectory());
+		List<File> files = Arrays.asList(dir.listFiles());
+		logger.info("LocalReportStorageApi.init 4: dir files = " + files.stream().map(f->f.getAbsolutePath()).toList());
 	}
 
 	public DownloadFileResult readFile(String fileName) throws IOException {
 		logger.info("LocalReportStorageApi.readFile 1: reportsDirPath = " + reportsDirPath + ", fileName = " + fileName);
 		Path path = Paths.get(reportsDirPath, fileName);
-		logger.info("LocalReportStorageApi.readFile 2: path " + path.toAbsolutePath());
-		logger.info("LocalReportStorageApi.readFile 2: path " + path.toFile());
+		logger.info("LocalReportStorageApi.readFile 2.1: path " + path.toAbsolutePath());
+		logger.info("LocalReportStorageApi.readFile 2.2: file.exists = " + path.toFile().exists());
 		long fileSize = path.toFile().length();
+		logger.info("LocalReportStorageApi.readFile 2.3: file.size = " + fileSize);
 		InputStream body = new FileInputStream(path.toFile());
 		DownloadFileResult r = new DownloadFileResult(fileName, body, fileSize);
 		logger.info("LocalReportStorageApi.readFile 3: r " + r.getName());
