ec3404cf9b122aa3007d0a3ea7d49c246ccde47d
Or Bartal <orbartal@gmail.com>
2023-05-25 14:02:39 -0400

    Added allot of logs to LocalReportStorageApi

--- src/main/java/esense/bff/report/LocalReportStorageApi.java
+++ src/main/java/esense/bff/report/LocalReportStorageApi.java
@@ -6,6 +6,8 @@ import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,8 @@ import jakarta.annotation.PostConstruct;
 
 @Service
 public class LocalReportStorageApi {
+
+	private static final Logger logger = LoggerFactory.getLogger(LocalReportStorageApi.class);
 
 	@Value("${report.storage.dir}")
 	private String reportsDirPath;
@@ -29,13 +33,19 @@ public class LocalReportStorageApi {
 	private void init() {
 		dirValidator.createIfNotExists(reportsDirPath);
 		dirValidator.validateDirPath(reportsDirPath);
+		logger.info("LocalReportStorageApi.init: reportsDirPath " + reportsDirPath);
 	}
 
 	public DownloadFileResult readFile(String fileName) throws IOException {
+		logger.info("LocalReportStorageApi.readFile 1: reportsDirPath = " + reportsDirPath + ", fileName = " + fileName);
 		Path path = Paths.get(reportsDirPath, fileName);
+		logger.info("LocalReportStorageApi.readFile 2: path " + path.toAbsolutePath());
+		logger.info("LocalReportStorageApi.readFile 2: path " + path.toFile());
 		long fileSize = path.toFile().length();
 		InputStream body = new FileInputStream(path.toFile());
-		return new DownloadFileResult(fileName, body, fileSize);
+		DownloadFileResult r = new DownloadFileResult(fileName, body, fileSize);
+		logger.info("LocalReportStorageApi.readFile 3: r " + r.getName());
+		return r;
 	}
 
 }
