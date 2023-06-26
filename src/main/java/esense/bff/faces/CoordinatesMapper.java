66fa05e47b772e9b70bbb9173cf8c84d48ed4025
Or Bartal <orbartal@gmail.com>
2023-06-26 00:10:50 -0400

    Small changes in CoordinatesMapper

--- src/main/java/esense/bff/faces/CoordinatesMapper.java
+++ src/main/java/esense/bff/faces/CoordinatesMapper.java
@@ -12,13 +12,17 @@ public class CoordinatesMapper {
 		if (input == null || input.length() < 3) {
 			return new ArrayList<>();
 		}
-		String str1 = input.substring(1, input.length() - 1);
+		String input2 = input.strip();
+		System.out.println("input2 = " + input2);
+		String str1 = input.substring(1, input2.length() - 1);
+		System.out.println("str1 = " + str1);
 		List<String> coordinatesAsText = Arrays.asList(str1.split(","));
 		System.out.println("coordinatesAsText = " + coordinatesAsText);
 		List<List<Integer>> numbers = new ArrayList<>();
 		for (String text : coordinatesAsText) {
 			System.out.println("text = " + text);
-			String str2 = text.replace("[", "").replace("]", "");
+			String str2 = text.substring(1, input2.length() - 1);
+			System.out.println("str2 = " + str2);
 			List<String> coordinatesAsText2 = Arrays.asList(str2.split(","));
 			System.out.println("coordinatesAsText2 = " + coordinatesAsText2);
 			List<Integer> coordinatesAsIntegers = coordinatesAsText2.stream().map(s -> Integer.parseInt(s.strip()))
